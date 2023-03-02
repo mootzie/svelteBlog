e9044e57392940ce1e576cb2f3c3d710f5448360
mootzie <tylerw77156@gmail.com>
2023-03-02 08:25:47 -0600

    added netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,21 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
+
 const config = {
 	kit: {
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+	   
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		  }),
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		
 	}
 };
 
